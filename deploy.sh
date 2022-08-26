d8dc33eef2e5f9dc4a3cfd067529a592e5890ad0
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2022-08-26 17:59:21 +0200

    fix

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:pierrecamilleri/hugo_docs_test.git master:gh-pages
+git push -f git@github.com:pierrecamilleri/hugo_docs_test.git main:gh-pages
 
 cd -
