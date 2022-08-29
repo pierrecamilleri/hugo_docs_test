29bd6451c75f630ba82956faf96eef725f774451
Pierre Camilleri <22995923+pierrecamilleri@users.noreply.github.com>
2022-08-29 15:02:44 +0200

    Github actions depoy automation

---
+++ .github/workflow/vuepress-deploy.sh
@@ -0,0 +1,17 @@
+name: Build and Deploy
+on: [push]
+jobs:
+  build-and-deploy:
+    runs-on: ubuntu-latest
+    steps:
+    - name: Checkout
+      uses: actions/checkout@master
+
+    - name: vuepress-deploy
+      uses: jenkey2011/vuepress-deploy@master
+      env:
+        ACCESS_TOKEN: ${{ secrets.ACCESS_TOKEN }}
+        TARGET_REPO: pierrecamilleri/hugo_docs_test
+        TARGET_BRANCH: main
+        BUILD_SCRIPT: npm run build
+        BUILD_DIR: ./docs/
